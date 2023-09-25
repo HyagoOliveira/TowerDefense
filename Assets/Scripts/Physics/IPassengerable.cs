e656cbd71d8786947b49e3b86c6cec477e78b682
Hyago Oliveira <hyagogow@gmail.com>
2023-09-25 19:06:00 -0300

    Add IPassengerable interface

---
+++ Assets/Scripts/Physics/IPassengerable.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using TowerDefense.VisualEffects;
+
+namespace TowerDefense.Physics
+{
+    /// <summary>
+    /// Interface for objects able to be a passenger.
+    /// </summary>
+    public interface IPassengerable
+    {
+        public Transform transform { get; }
+        public MaterialChanger MaterialChanger { get; }
+    }
+}
