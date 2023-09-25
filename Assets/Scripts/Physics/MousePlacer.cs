1b9aa906472e7528522c2a795341666676bd0fdd
Hyago Oliveira <hyagogow@gmail.com>
2023-09-25 19:07:46 -0300

    Move Passenger

--- Assets/Scripts/Physics/MousePlacer.cs
+++ Assets/Scripts/Physics/MousePlacer.cs
@@ -6,17 +6,46 @@ namespace TowerDefense.Physics
     [DisallowMultipleComponent]
     public sealed class MousePlacer : MonoBehaviour
     {
+        [Header("Physics Detection")]
         [SerializeField] private LayerMask collision;
+        [SerializeField, Min(0f)] private float maxDistance = 100F;
+
+        [Header("Input")]
+        [SerializeField] private string placePassagerButtontName = "Place Defender";
+
+        [Header("Passenger")]
+        [SerializeField] private Vector3 passengerOffset = Vector3.up * 0.5F;
+
+        [Header("Feedbacks")]
+        [SerializeField] private Material failFeedback;
+        [SerializeField] private Material successFeedback;
+
+        public bool HasValidPosition { get; private set; }
 
         private Camera mainCamera;
-        private const float maxDistance = 100F;
+        private IPassengerable passenger;
 
         private void Awake() => mainCamera = Camera.main;
-        private void Start() => enabled = false;
+        private void Start() => enabled = false; // For optimization
 
         private void Update()
         {
             UpdatePositionUsingMouse();
+
+            if (passenger != null)
+            {
+                UpdatePassengerMaterialFeedback();
+                UpdatePlaceInput();
+            }
+        }
+
+        public void SetPassenger(IPassengerable passenger)
+        {
+            this.passenger = passenger;
+            this.passenger.transform.SetParent(transform);
+            this.passenger.transform.position = passengerOffset;
+
+            enabled = true; // For optimization
         }
 
         private void UpdatePositionUsingMouse()
@@ -24,9 +53,37 @@ namespace TowerDefense.Physics
             var cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
             var hasHit = UnityPhysics.Raycast(cameraRay, out RaycastHit hit, maxDistance, collision);
 
+            HasValidPosition = hasHit && CanPlacePassenger();
+
             if (!hasHit) return;
 
             transform.position = hit.point;
         }
+
+        private void UpdatePlaceInput()
+        {
+            var hasInput = Input.GetButtonDown(placePassagerButtontName);
+            if (hasInput && HasValidPosition) PlacePassager();
+        }
+
+        private void UpdatePassengerMaterialFeedback()
+        {
+            var material = HasValidPosition ? successFeedback : failFeedback;
+            passenger.MaterialChanger.SetMaterials(material);
+        }
+
+        private void PlacePassager()
+        {
+            passenger.MaterialChanger.ResetMaterials();
+
+            passenger.transform.SetParent(null);
+            passenger.transform.position -= passengerOffset;
+
+            passenger = null;
+
+            enabled = false; // For optimization
+        }
+
+        private bool CanPlacePassenger() => true;
     }
 }
