04648fe680b4093084f60660c04a848a34cd989c
Hyago Oliveira <hyagogow@gmail.com>
2023-09-25 19:07:23 -0300

    Implement IPassengerable

--- Assets/Scripts/Gameplay/Defender.cs
+++ Assets/Scripts/Gameplay/Defender.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
+using TowerDefense.Physics;
 using TowerDefense.VisualEffects;
 
 namespace TowerDefense.Gameplay
 {
     [DisallowMultipleComponent]
     [RequireComponent(typeof(MaterialChanger))]
-    public sealed class Defender : MonoBehaviour
+    public sealed class Defender : MonoBehaviour, IPassengerable
     {
         [field: SerializeField] public MaterialChanger MaterialChanger { get; private set; }
 
