2d50413edb7d4b24cd3533ad62f0be3e947109e5
Hyago Oliveira <hyagogow@gmail.com>
2023-09-26 20:56:22 -0300

    Add DefenderButtonsGroup component/prefab

---
+++ Assets/Scripts/UI/DefenderButtonsGroup.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using TowerDefense.Managers;
+
+namespace TowerDefense.UI
+{
+    [DisallowMultipleComponent]
+    public sealed class DefenderButtonsGroup : MonoBehaviour
+    {
+        [SerializeField] private DefenderButton prefab;
+        [SerializeField] private MatchSettings settings;
+
+        private DefenderButton[] buttons;
+
+        private void OnEnable() => settings.OnStarted += HandleMatchStarted;
+        private void OnDisable() => settings.OnStarted -= HandleMatchStarted;
+
+        private void HandleMatchStarted() => ResetButtons();
+
+        private void ResetButtons()
+        {
+            Clear();
+            Instantiate();
+        }
+
+        private void Instantiate()
+        {
+            buttons = new DefenderButton[settings.Defenders.Length];
+
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i] = Instantiate(prefab);
+                buttons[i].Initialize(i, transform);
+            }
+        }
+
+        private void Clear()
+        {
+            foreach (var button in buttons)
+            {
+                Destroy(button.gameObject);
+            }
+
+            buttons = new DefenderButton[0];
+        }
+    }
+}
