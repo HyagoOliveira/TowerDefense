631f8b5b465564ecab7f7633bc1b519abf3a1655
Hyago Oliveira <hyagogow@gmail.com>
2023-09-26 11:00:13 -0300

    Add AbstractDynamicViewer component

---
+++ Assets/Scripts/UI/DynamicViewers/AbstractDynamicViewer.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+
+namespace TowerDefense.UI
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public abstract class AbstractDynamicViewer : MonoBehaviour
+    {
+        [SerializeField] protected TMP_Text textMesh;
+        [SerializeField] protected MatchSettings settings;
+        [SerializeField] protected string format = "D2";
+
+        private void Reset() => textMesh = GetComponent<TMP_Text>();
+        private void OnEnable() => GetValue().OnChanged += HandleValueChanged;
+        private void OnDisable() => GetValue().OnChanged -= HandleValueChanged;
+
+        private void HandleValueChanged(int value) => textMesh.text = value.ToString(format);
+
+        protected abstract DynamicValue<int> GetValue();
+    }
+}
