e5bb6236cdfcc19b1bf9bc956ae7b1aada4df168
Hyago Oliveira <hyagogow@gmail.com>
2023-09-26 07:24:47 -0300

    Add DefenseButton component

---
+++ Assets/Scripts/UI/DefenseButton.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TowerDefense.Physics;
+using TowerDefense.Gameplay;
+
+namespace TowerDefense.UI
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(Button))]
+    public sealed class DefenseButton : MonoBehaviour
+    {
+        [SerializeField] private Button button;
+        [SerializeField] private Defender defender;
+
+        private void Reset() => button = GetComponent<Button>();
+        private void OnEnable() => button.onClick.AddListener(HandleButtonClicked);
+        private void OnDisable() => button.onClick.RemoveListener(HandleButtonClicked);
+
+        private void HandleButtonClicked() => SpawnDefender();
+
+        //TODO move code elsewhere
+        private void SpawnDefender()
+        {
+            var placer = FindObjectOfType<MousePlacer>();
+            var instance = Instantiate(defender);
+
+            placer.SetPassenger(instance);
+        }
+    }
+}
