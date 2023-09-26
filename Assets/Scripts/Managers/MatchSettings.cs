11d2b196865839ea4a58c7b6b98f6848e78623b6
Hyago Oliveira <hyagogow@gmail.com>
2023-09-26 11:20:39 -0300

    Add Round Dynamic value

--- Assets/Scripts/Managers/MatchSettings.cs
+++ Assets/Scripts/Managers/MatchSettings.cs
@@ -9,12 +9,14 @@ namespace TowerDefense.Managers
         [SerializeField] private int initialHealth = 100;
         [SerializeField] private int initialCurrency = 500;
 
+        public DynamicValue<int> Round { get; private set; } = new DynamicValue<int>();
         public DynamicValue<int> Score { get; private set; } = new DynamicValue<int>();
         public DynamicValue<int> Health { get; private set; } = new DynamicValue<int>();
         public DynamicValue<int> Currency { get; private set; } = new DynamicValue<int>();
 
         internal void ResetValues()
         {
+            Round.Value = 0;
             Score.Value = initialScore;
             Health.Value = initialHealth;
             Currency.Value = initialCurrency;
