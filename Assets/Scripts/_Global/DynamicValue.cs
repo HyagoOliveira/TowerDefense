d49173bdd8065ed5146209b7642a42f3362b3ea3
Hyago Oliveira <hyagogow@gmail.com>
2023-09-26 10:43:32 -0300

    Add DynamicValue class

---
+++ Assets/Scripts/_Global/DynamicValue.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace TowerDefense
+{
+    public sealed class DynamicValue<T> where T : IEquatable<T>
+    {
+        /// <summary>
+        /// Event fired when the current <see cref="Value"/> is changed.
+        /// </summary>
+        public event Action<T> OnChanged;
+
+        /// <summary>
+        /// The current value.
+        /// </summary>
+        public T Value
+        {
+            get => value;
+            internal set
+            {
+                if (this.value.Equals(value)) return;
+
+                this.value = value;
+                OnChanged?.Invoke(this.value);
+            }
+        }
+
+        private T value;
+
+        public DynamicValue(T value) => this.value = value;
+    }
+}
