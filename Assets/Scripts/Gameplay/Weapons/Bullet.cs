ad36d4d0c5e7f6f132e83fc032648f809af28f0c
Hyago Oliveira <hyagogow@gmail.com>
2023-09-27 15:35:54 -0300

    Add Bullet component

---
+++ Assets/Scripts/Gameplay/Weapons/Bullet.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace TowerDefense.Gameplay
+{
+    [DisallowMultipleComponent]
+    public sealed class Bullet : MonoBehaviour
+    {
+        [SerializeField, Min(0f)] private float speed = 1f;
+        [SerializeField, Min(0f)] private float timeAlive = 10f;
+        [SerializeField] private Vector3 direction = Vector3.forward;
+
+        private void Start() => Invoke(nameof(Destroy), timeAlive);
+        private void FixedUpdate() => UpdateMovement();
+
+        public void Fire(float speed, Vector3 direction)
+        {
+            this.speed = speed;
+            this.direction = direction;
+        }
+
+        private void UpdateMovement()
+        {
+            var speedPerSeconds = speed * Time.deltaTime;
+            var velocity = speedPerSeconds * direction;
+
+            transform.position += velocity;
+        }
+
+        private void Destroy() => Destroy(gameObject);
+    }
+}
