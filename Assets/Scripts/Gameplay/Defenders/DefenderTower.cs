4b10cb9ee0ec556fd8511e081564fcd8792e9f45
Hyago Oliveira <hyagogow@gmail.com>
2023-09-27 22:03:33 -0300

    Add Weapon component

--- Assets/Scripts/Gameplay/Defenders/DefenderTower.cs
+++ Assets/Scripts/Gameplay/Defenders/DefenderTower.cs
@@ -1,24 +1,30 @@
 using UnityEngine;
 using UnityEngine.AI;
+using TowerDefense.Physics;
 using TowerDefense.VisualEffects;
 
 namespace TowerDefense.Gameplay
 {
     [DisallowMultipleComponent]
     [RequireComponent(typeof(BoxCollider))]
+    [RequireComponent(typeof(TowerDetector))]
     [RequireComponent(typeof(NavMeshObstacle))]
     [RequireComponent(typeof(MaterialReplacer))]
     public sealed class DefenderTower : MonoBehaviour
     {
+        [SerializeField] private TowerDetector detector;
         [SerializeField] private BoxCollider boxCollider;
         [SerializeField] private MaterialReplacer material;
 
         public int Price => 10;
         public string Name => gameObject.name;
+
+        public TowerDetector Detector => detector;
         public MaterialReplacer Material => material;
 
         private void Reset()
         {
+            detector = GetComponent<TowerDetector>();
             boxCollider = GetComponent<BoxCollider>();
             material = GetComponent<MaterialReplacer>();
         }
---
+++ Assets/Scripts/Gameplay/Weapons/Weapon.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using UnityEngine;
+
+namespace TowerDefense.Gameplay
+{
+    [DisallowMultipleComponent]
+    public class Weapon : MonoBehaviour
+    {
+        [SerializeField, Min(0)] protected int damage;
+        [SerializeField, Min(0f)] protected float reloadTime;
+
+        [Space]
+        [SerializeField] private Bullet bulletPrefab;
+        [SerializeField] private Transform bulletSpawnPoint;
+        [SerializeField, Min(0f)] private float bulletSpeed;
+
+        private DefenderTower tower;
+
+        private void Awake() => tower = GetComponentInParent<DefenderTower>();
+        private void Start() => StartAttack();
+
+        public void StartAttack() => StartCoroutine(FireRoutine());
+        public void Stop() => StopAllCoroutines();
+
+        private IEnumerator FireRoutine()
+        {
+            var waitReloadTime = new WaitForSeconds(reloadTime);
+            var waitUntilTragetIsFound = new WaitUntil(IsTargetFound);
+
+            while (true)
+            {
+                yield return waitReloadTime;
+                yield return waitUntilTragetIsFound;
+                Fire();
+            }
+        }
+
+        protected virtual void Fire()
+        {
+            //TODO improve using a PoolSystem
+            var bullet = Instantiate(
+                bulletPrefab,
+                bulletSpawnPoint.position,
+                rotation: Quaternion.identity
+            );
+            var bulletDirection =
+                (tower.Detector.TargetPosition - bullet.transform.position).normalized;
+
+            bullet.Fire(
+                bulletSpeed,
+                bulletDirection,
+                damage
+            );
+        }
+
+        private bool IsTargetFound() => tower.Detector.HasTarget;
+    }
+}
