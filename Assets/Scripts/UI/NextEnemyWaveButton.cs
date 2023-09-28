583eb32812d9ea0d816e5e90d4c19351804d1272
Hyago Oliveira <hyagogow@gmail.com>
2023-09-28 13:58:46 -0300

    Disable Next Wave Button while wave is been spawned

--- Assets/Scripts/UI/NextEnemyWaveButton.cs
+++ Assets/Scripts/UI/NextEnemyWaveButton.cs
@@ -12,9 +12,23 @@ namespace TowerDefense.UI
         [SerializeField] private MatchSettings settings;
 
         private void Reset() => button = GetComponent<Button>();
-        private void OnEnable() => button.onClick.AddListener(HandleButtonClicked);
-        private void OnDisable() => button.onClick.RemoveListener(HandleButtonClicked);
+
+        private void OnEnable()
+        {
+            button.onClick.AddListener(HandleButtonClicked);
+            settings.OnAnyEnemyWaveStarted += HandleEnemyWaveStarted;
+            settings.OnAnyEnemyWaveFinished += HandleEnemyWaveFinished;
+        }
+
+        private void OnDisable()
+        {
+            button.onClick.RemoveListener(HandleButtonClicked);
+            settings.OnAnyEnemyWaveStarted -= HandleEnemyWaveStarted;
+            settings.OnAnyEnemyWaveFinished -= HandleEnemyWaveFinished;
+        }
 
         private void HandleButtonClicked() => settings.SpawnNextEnemyWave();
+        private void HandleEnemyWaveStarted() => button.interactable = false;
+        private void HandleEnemyWaveFinished() => button.interactable = true;
     }
 }
