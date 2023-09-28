bba0c27bb100b2f2416abb7770c79f2a17a18b53
Hyago Oliveira <hyagogow@gmail.com>
2023-09-28 12:17:11 -0300

    Remove EnemySpawner.Spawn function

--- Assets/Scripts/Gameplay/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Gameplay/Enemies/EnemySpawner.cs
@@ -1,21 +1,11 @@
 using UnityEngine;
-using UnityEngine.AI;
 
 namespace TowerDefense.Gameplay
 {
     [DisallowMultipleComponent]
     public sealed class EnemySpawner : MonoBehaviour
     {
-        [SerializeField] private GameObject prefab;
         [SerializeField] private Transform origin;
         [SerializeField] private Transform destination;
-
-        public void Spawn()
-        {
-            var agent = Instantiate(prefab).GetComponent<NavMeshAgent>();
-
-            agent.transform.position = origin.position;
-            agent.SetDestination(destination.position);
-        }
     }
 }
