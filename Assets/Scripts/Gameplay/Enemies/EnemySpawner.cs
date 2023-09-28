c6a9fb97549c09529eb2103edc0e88d49b5d507f
Hyago Oliveira <hyagogow@gmail.com>
2023-09-28 12:25:54 -0300

    Spawn enemies waves

--- Assets/Scripts/Gameplay/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Gameplay/Enemies/EnemySpawner.cs
@@ -1,11 +1,25 @@
 using UnityEngine;
+using TowerDefense.Managers;
 
 namespace TowerDefense.Gameplay
 {
     [DisallowMultipleComponent]
     public sealed class EnemySpawner : MonoBehaviour
     {
+        [SerializeField] private MatchSettings settings;
         [SerializeField] private Transform origin;
         [SerializeField] private Transform destination;
+
+        private void OnEnable() => settings.OnEnemyWaveSpawned += HandleEnemyWaveSpawned;
+        private void OnDisable() => settings.OnEnemyWaveSpawned -= HandleEnemyWaveSpawned;
+
+        private void HandleEnemyWaveSpawned(EnemyWave wave)
+        {
+            var direction = (destination.position - origin.position).normalized;
+            var orientation = Quaternion.LookRotation(direction);
+
+            StopAllCoroutines();
+            StartCoroutine(wave.SpawnRoutine(origin.position, destination.position, orientation));
+        }
     }
 }
