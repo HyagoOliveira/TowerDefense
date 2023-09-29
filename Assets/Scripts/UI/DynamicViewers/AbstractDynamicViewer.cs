99a224746d2f581d8451dfb7187ab0f7a4004e42
Hyago Oliveira <hyagogow@gmail.com>
2023-09-29 16:28:15 -0300

    Show Increased/Decreased values from dynamic views

--- Assets/Scripts/UI/DynamicViewers/AbstractDynamicViewer.cs
+++ Assets/Scripts/UI/DynamicViewers/AbstractDynamicViewer.cs
@@ -1,29 +1,81 @@
 using TMPro;
 using UnityEngine;
 using TowerDefense.Managers;
+using System.Collections;
 
 namespace TowerDefense.UI
 {
     [RequireComponent(typeof(TMP_Text))]
     public abstract class AbstractDynamicViewer : MonoBehaviour
     {
-        [SerializeField] protected TMP_Text textMesh;
         [SerializeField] protected MatchSettings settings;
+
+        [Space]
+        [SerializeField] protected TMP_Text currentValue;
+        [SerializeField] protected TMP_Text increasedValue;
+        [SerializeField] protected TMP_Text decreasedValue;
+
+        [Space]
         [SerializeField] protected string spriteName;
         [SerializeField] protected string format = "D2";
 
-        private void Reset() => textMesh = GetComponent<TMP_Text>();
-        private void OnEnable() => GetValue().OnChanged += HandleValueChanged;
-        private void OnDisable() => GetValue().OnChanged -= HandleValueChanged;
+        private readonly WaitForSeconds waitOneSecond = new WaitForSeconds(1);
+
+        private void Reset() => currentValue = GetComponent<TMP_Text>();
+
+        private void Awake()
+        {
+            increasedValue.enabled = false;
+            decreasedValue.enabled = false;
+        }
+
+        private void OnEnable()
+        {
+            GetValue().OnChanged += HandleValueChanged;
+            GetValue().OnIncreased += HandleValueIncreased;
+            GetValue().OnDecreased += HandleValueDecreased;
+        }
+
+        private void OnDisable()
+        {
+            GetValue().OnChanged -= HandleValueChanged;
+            GetValue().OnIncreased -= HandleValueIncreased;
+            GetValue().OnDecreased -= HandleValueDecreased;
+        }
+
+        protected abstract DynamicInteger GetValue();
 
         private void HandleValueChanged(int value)
         {
             var result = value.ToString(format);
             var textWithSprite = $"<sprite=\"Icons\" name=\"{spriteName}\"> {result}";
 
-            textMesh.text = textWithSprite;
+            currentValue.text = textWithSprite;
         }
 
-        protected abstract DynamicInteger GetValue();
+        private void HandleValueIncreased(int increase)
+        {
+            increasedValue.text = $"+ {increase}";
+            StartCoroutine(ShowValueRoutine(increasedValue));
+        }
+
+        private void HandleValueDecreased(int decrease)
+        {
+            decreasedValue.text = $"- {decrease}";
+            StartCoroutine(ShowValueRoutine(decreasedValue));
+        }
+
+        private IEnumerator ShowValueRoutine(TMP_Text text)
+        {
+            text.enabled = true;
+
+            //TODO play fade out animation
+
+            yield return waitOneSecond;
+
+            //TODO play fade in animation
+
+            text.enabled = false;
+        }
     }
 }
