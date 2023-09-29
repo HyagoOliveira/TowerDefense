e4dce515f6e8f2625dceafcdba585e6664a7fb23
Hyago Oliveira <hyagogow@gmail.com>
2023-09-29 15:34:59 -0300

    Improve DynamicValue

--- Assets/Scripts/Utils/DynamicValue.cs
+++ Assets/Scripts/Utils/DynamicValue.cs
@@ -2,20 +2,25 @@ using System;
 
 namespace TowerDefense
 {
-    public sealed class DynamicValue<T> where T : IEquatable<T>
+    /// <summary>
+    /// Dynamic value with <see cref="OnChanged"/> event.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class DynamicValue<T> where T : IEquatable<T>, IComparable<T>
     {
         /// <summary>
-        /// Event fired when the current <see cref="Value"/> is changed.
+        /// Event fired when the <see cref="Value"/> is changed.
+        /// <para>The given param is <b>the new value</b>.</para>
         /// </summary>
         public event Action<T> OnChanged;
 
         /// <summary>
         /// The current value.
         /// </summary>
-        public T Value
+        public virtual T Value
         {
             get => value;
-            internal set
+            set
             {
                 if (this.value.Equals(value)) return;
 
@@ -24,6 +29,12 @@ namespace TowerDefense
             }
         }
 
-        private T value;
+        protected T value;
+
+        /// <summary>
+        /// Initializes the dynamic value <b>without raising</b> the <see cref="OnChanged"/> event.
+        /// </summary>
+        /// <param name="value">The current dynamic value.</param>
+        public DynamicValue(T value) => this.value = value;
     }
 }
