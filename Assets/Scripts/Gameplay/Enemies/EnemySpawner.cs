6079d909d6bc72d984a8e49e858d3669a63fa48d
Hyago Oliveira <hyagogow@gmail.com>
2023-09-29 09:02:48 -0300

    Add IsAbleToCompletePath function

--- Assets/Scripts/Gameplay/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Gameplay/Enemies/EnemySpawner.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.AI;
 using TowerDefense.Managers;
 
 namespace TowerDefense.Gameplay
@@ -7,19 +8,31 @@ namespace TowerDefense.Gameplay
     public sealed class EnemySpawner : MonoBehaviour
     {
         [SerializeField] private MatchSettings settings;
+        [SerializeField] private NavMeshAgent pathAgent;
         [SerializeField] private Transform origin;
         [SerializeField] private Transform destination;
+
+        public Vector3 Origin => origin.position;
+        public Vector3 Destination => destination.position;
 
         private void OnEnable() => settings.OnEnemyWaveSpawned += HandleEnemyWaveSpawned;
         private void OnDisable() => settings.OnEnemyWaveSpawned -= HandleEnemyWaveSpawned;
 
+        public bool IsAbleToCompletePath()
+        {
+            var path = new NavMeshPath();
+            return
+                pathAgent.CalculatePath(Destination, path) &&
+                path.status == NavMeshPathStatus.PathComplete;
+        }
+
         private void HandleEnemyWaveSpawned(EnemyWave wave)
         {
-            var direction = (destination.position - origin.position).normalized;
+            var direction = (Destination - Origin).normalized;
             var orientation = Quaternion.LookRotation(direction);
 
             StopAllCoroutines();
-            StartCoroutine(wave.SpawnRoutine(origin.position, destination.position, orientation));
+            StartCoroutine(wave.SpawnRoutine(Origin, Destination, orientation));
         }
     }
 }
